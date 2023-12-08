a507d6eab6e5e1966319563e55f43b26bdbe9f57
Julian Gödde <goedde@tuta.io>
2023-12-08 11:57:00 +0100

    Refactor PubgReportHostedService class for improved readiness check
    Introduced a readiness check in the PubgReportHostedService class. This change helps to ensure the DiscordSocketClient is ready before attempting to fetch stream information. Also, the variables streamInfoService and discordSocketClient have been made private readonly, and a constant for interval minutes has been established.

--- HostedServices/PubgReportHostedService.cs
+++ HostedServices/PubgReportHostedService.cs
@@ -9,15 +9,31 @@ using PubgReportCrawler.ValueObjects;
 
 namespace PubgReportCrawler.HostedServices;
 
-public class PubgReportHostedService(
-    StreamInfoService streamInfoService,
-    DiscordSocketClient discordSocketClient) : IHostedService, IDisposable
+public class PubgReportHostedService : IHostedService, IDisposable
 {
     private Timer? _timer;
+    private const int EveryXMinutes = 30;
+    private bool _isReady;
+
+    private readonly StreamInfoService _streamInfoService;
+    private readonly DiscordSocketClient _discordSocketClient;
+
+    public PubgReportHostedService(StreamInfoService streamInfoService,
+        DiscordSocketClient discordSocketClient)
+    {
+        _streamInfoService = streamInfoService;
+        _discordSocketClient = discordSocketClient;
+
+        discordSocketClient.Ready += () =>
+        {
+            _isReady = true;
+            return Task.CompletedTask;
+        };
+    }
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
-        _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromMinutes(30));
+        _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromMinutes(EveryXMinutes));
 
         return Task.CompletedTask;
     }
@@ -36,13 +52,21 @@ public class PubgReportHostedService(
 
     private async void DoWork(object? state)
     {
-        await streamInfoService.GetStreamInfo(new PubgReportAccountId("account.1eb237a0e35b45d7a041b76681851604"),
+        if (!_isReady)
+        {
+            _timer?.Change(TimeSpan.FromSeconds(2), TimeSpan.FromSeconds(10));
+            return;
+        }
+
+        _timer?.Change(TimeSpan.FromMinutes(EveryXMinutes), TimeSpan.FromMinutes(EveryXMinutes));
+
+        await _streamInfoService.GetStreamInfo(new PubgReportAccountId("account.1eb237a0e35b45d7a041b76681851604"),
             OnNewStreamInfo);
     }
 
     private async void OnNewStreamInfo(IReadOnlyList<StreamInfo> info)
     {
-        IUser? socketUser = await discordSocketClient.GetUserAsync(281128906243702784);
+        IUser? socketUser = await _discordSocketClient.GetUserAsync(281128906243702784);
         if (socketUser is null)
         {
             return;
