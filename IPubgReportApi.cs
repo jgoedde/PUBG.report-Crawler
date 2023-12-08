7ea7989b5d32de8524a96e1b5373c0e503e5a4d4
Julian Gödde <goedde@tuta.io>
2023-12-08 09:47:40 +0100

    Add PubgReportApi interface
    A new interface, IPubgReportApi, was added to interact with the PUBG Report API. This interface includes a method to retrieve streams for a given PubgReportAccountId, which returns an asynchronous operation that gives a GetStreamsResponse object.

---
+++ IPubgReportApi.cs
@@ -0,0 +1,18 @@
+using PubgReportCrawler.Dtos;
+using Refit;
+
+namespace PubgReportCrawler;
+
+/// <summary>
+/// Represents the interface for interacting with the PUBG Report API.
+/// </summary>
+public interface IPubgReportApi
+{
+    /// <summary>
+    /// Retrieves the streams for a given PubgReportAccountId.
+    /// </summary>
+    /// <param name="id">The PubgReportAccountId of the player.</param>
+    /// <returns>An asynchronous operation that returns a GetStreamsResponse object.</returns>
+    [Get("/players/{id.Value}/streams")]
+    Task<GetStreamsResponse> GetStreams(PubgReportAccountId id);
+}
