cd9f70db555499413e2ce5d5b5a1a30bfe456558
Julian Gödde <goedde@tuta.io>
2023-12-08 09:49:49 +0100

    Set up Dependency Injection and Hosting for Pubg API
    The initial console program has been updated to a host builder pattern using .NET generic host. We've added Dependency Injection (DI) services via Microsoft.Extensions.DependencyInjection and even set up a client service for a Pubg Report API using Refit.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PubgReportCrawler;
+using Refit;
+
+// create hosting object and DI layer
+using var host = CreateHostBuilder(args).Build();
+
+// create a service scope
+using var scope = host.Services.CreateScope();
+
+IHostBuilder CreateHostBuilder(string[] strings)
+{
+    return Host.CreateDefaultBuilder()
+        .ConfigureServices((_, services) =>
+        {
+            services.AddRefitClient<IPubgReportApi>()
+                .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://api.pubg.report/v1"));
+        });
+}
+
+await host.RunAsync();
