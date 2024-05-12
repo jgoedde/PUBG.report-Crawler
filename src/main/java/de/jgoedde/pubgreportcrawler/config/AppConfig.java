8b87967030d5bccfd2bcf7528a9e2d966ac30935
Julian Gödde <goedde@tuta.io>
2024-05-12 12:23:38 +0200

    🔧 Add app settings

---
+++ src/main/java/de/jgoedde/pubgreportcrawler/config/AppConfig.java
@@ -0,0 +1,12 @@
+package de.jgoedde.pubgreportcrawler.config;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "app")
+@Getter
+@RequiredArgsConstructor
+public final class AppConfig {
+    private final String pubgReportApiUrl;
+}
