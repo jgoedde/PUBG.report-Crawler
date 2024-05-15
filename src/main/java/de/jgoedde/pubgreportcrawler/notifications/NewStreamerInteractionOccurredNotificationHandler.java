d7ba36022b4db86eb8ce3ba0feeda4f2e25a4026
Julian Gödde <goedde@tuta.io>
2024-05-15 21:04:12 +0200

    ✨ Implement sending discord private message on new streamer interaction

--- src/main/java/de/jgoedde/pubgreportcrawler/notifications/NewStreamerInteractionOccurredNotificationHandler.java
+++ src/main/java/de/jgoedde/pubgreportcrawler/notifications/NewStreamerInteractionOccurredNotificationHandler.java
@@ -1,15 +1,32 @@
 package de.jgoedde.pubgreportcrawler.notifications;
 
 import an.awesome.pipelinr.Notification;
+import de.jgoedde.pubgreportcrawler.config.DiscordBotConfig;
+import discord4j.common.util.Snowflake;
+import discord4j.core.object.entity.User;
+import discord4j.core.spec.MessageCreateSpec;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.stereotype.Component;
 
 @Component
 @Log
+@RequiredArgsConstructor
 public class NewStreamerInteractionOccurredNotificationHandler implements Notification.Handler<NewStreamerInteractionOccurredNotification> {
+    private final DiscordBotConfig discordBot;
+    private final ApplicationArguments args;
+
     @Override
     public void handle(NewStreamerInteractionOccurredNotification notification) {
-        log.info("Should send discord message to Me");
-        // TODO: Send discord DM
+        String message = "neue Streamer-Interaktionen: https://pubg.report/players/" + notification.kraftonAccountId();
+        long discordUserId = Long.parseLong(args.getOptionValues("discord-id").getFirst());
+
+        discordBot.gatewayDiscordClient().getUserById(Snowflake.of(discordUserId))
+                .flatMap(User::getPrivateChannel)
+                .flatMap(channel -> channel.createMessage(MessageCreateSpec.builder()
+                        .content(message)
+                        .build()))
+                .subscribe();
     }
 }
