744ae178eb5346c55a4bbef8bdaedc8eeaa8c108
Julian Gödde <129423545+jgoedde@users.noreply.github.com>
2024-09-02 18:27:21 +0200

    Add Debug Discord message on app startup

--- HostedServices/DiscordHostedService.cs
+++ HostedServices/DiscordHostedService.cs
@@ -13,10 +13,24 @@ public class DiscordHostedService(DiscordSocketClient discordClient) : IHostedSe
         await discordClient.LoginAsync(TokenType.Bot,
             "MTE2MDYxNzQyOTE1MjU2NzU1OA.GnYWK7.cm-GzzfoMC4iPF-uxde7rYKKP3PWzOc2TQB8TA");
         await discordClient.StartAsync();
+
+        IUser? socketUser = await _discordSocketClient.GetUserAsync(281128906243702784);
+        if (socketUser is null)
+        {
+            return;
+        }
+
+        await socketUser.SendMessageAsync("[PUBG.report Crawler] [Status] Der Discord-Bot ist gestartet.");
     }
 
     public async Task StopAsync(CancellationToken cancellationToken)
     {
+        IUser? socketUser = await _discordSocketClient.GetUserAsync(281128906243702784);
+        if (socketUser is not null)
+        {
+            await socketUser.SendMessageAsync("[PUBG.report Crawler] [Status] Der Bot stoppt nun.");
+        }
+
         await discordClient.StopAsync();
         await discordClient.LogoutAsync();
     }
