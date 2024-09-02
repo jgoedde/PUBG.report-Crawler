d19f3bc4b3ad7c1d87dce3047e960e0d4424a3c0
Julian Gödde <129423545+jgoedde@users.noreply.github.com>
2024-09-02 18:39:50 +0200

    Comment out sending message

--- HostedServices/DiscordHostedService.cs
+++ HostedServices/DiscordHostedService.cs
@@ -14,23 +14,26 @@ public class DiscordHostedService(DiscordSocketClient discordClient) : IHostedSe
             "MTE2MDYxNzQyOTE1MjU2NzU1OA.GnYWK7.cm-GzzfoMC4iPF-uxde7rYKKP3PWzOc2TQB8TA");
         await discordClient.StartAsync();
 
+/*
         IUser? socketUser = await discordClient.GetUserAsync(281128906243702784);
-        if (socketUser is null)
+        if (socketUser is not null)
         {
-            return;
+            await socketUser.SendMessageAsync("[PUBG.report Crawler] [Status] Der Discord-Bot ist gestartet.");
         }
 
-        await socketUser.SendMessageAsync("[PUBG.report Crawler] [Status] Der Discord-Bot ist gestartet.");
+        */
     }
 
     public async Task StopAsync(CancellationToken cancellationToken)
     {
+        /*
         IUser? socketUser = await discordClient.GetUserAsync(281128906243702784);
         if (socketUser is not null)
         {
             await socketUser.SendMessageAsync("[PUBG.report Crawler] [Status] Der Bot stoppt nun.");
         }
-
+        */
+        
         await discordClient.StopAsync();
         await discordClient.LogoutAsync();
     }
