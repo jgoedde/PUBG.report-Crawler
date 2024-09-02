e30b7198ee8213f3f4e32f2e99f8125a680d9eb5
Julian Gödde <129423545+jgoedde@users.noreply.github.com>
2024-09-02 18:29:41 +0200

    Fix variable name

--- HostedServices/DiscordHostedService.cs
+++ HostedServices/DiscordHostedService.cs
@@ -14,7 +14,7 @@ public class DiscordHostedService(DiscordSocketClient discordClient) : IHostedSe
             "MTE2MDYxNzQyOTE1MjU2NzU1OA.GnYWK7.cm-GzzfoMC4iPF-uxde7rYKKP3PWzOc2TQB8TA");
         await discordClient.StartAsync();
 
-        IUser? socketUser = await _discordSocketClient.GetUserAsync(281128906243702784);
+        IUser? socketUser = await discordClient.GetUserAsync(281128906243702784);
         if (socketUser is null)
         {
             return;
@@ -25,7 +25,7 @@ public class DiscordHostedService(DiscordSocketClient discordClient) : IHostedSe
 
     public async Task StopAsync(CancellationToken cancellationToken)
     {
-        IUser? socketUser = await _discordSocketClient.GetUserAsync(281128906243702784);
+        IUser? socketUser = await discordClient.GetUserAsync(281128906243702784);
         if (socketUser is not null)
         {
             await socketUser.SendMessageAsync("[PUBG.report Crawler] [Status] Der Bot stoppt nun.");
